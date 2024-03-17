330b63bd7e6ded7d6462f6a0fe51c76ad43298a0
jhayphix <jhayphix05@gmail.com>
2024-03-17 12:18:46 +0000

    Updated_lecture_practical_1

--- TimeSeriesScipts/Practical_1/Lecture_Practical_1.R
+++ TimeSeriesScipts/Practical_1/Lecture_Practical_1.R
@@ -1,53 +1,53 @@
-#----------------------------------------------
+# ----------------------------------------------
 # .... Imports
-#----------------------------------------------
+# ----------------------------------------------
 library(readxl)
 library(tseries)
 library(ggplot2)
 library(forecast)
-library(seastests)
+#library(seastests)
 
 # Read data
-data_path <- "~/RSpace/Datasources/Practicals_data.xls"
+data_path <- "//mac/Home/Documents/_DataScienceHub/R/Datasources/Practicals_data.xls"
 dataset <- read_excel(data_path)
 attach(dataset)
 
-#----------------------------------------------
+# ----------------------------------------------
 # .... Convert data to time series
-#----------------------------------------------
+# ----------------------------------------------
 Temp = ts(Temperature, start=1960, end=2018, frequency=1)
 Temp2= ts(Temperature, start=c(1960,1), frequency=12)
 
-#----------------------------------------------
-# .... Working with temp
-#----------------------------------------------
+# ----------------------------------------------
+# .... Working with temperature series
+# ----------------------------------------------
 BoxCox.lambda(Temp)
 
-autoplot(Temp2, main="Plot of Temperature two", col="maroon", lwd=2)
+autoplot(Temp, main="Plot of Temperature Series", col="maroon", lwd=2)
 
 adf.test(Temp)
 pp.test(Temp)
 kpss.test(Temp)
 
-# -----------------------------------------------------
-# --------------- Differencing ------------------------
-# -----------------------------------------------------
-Temp_first_diff = diff(Temp)
-Temp_first_diff
-plot(Temp_first_diff)
+# ----------------------------------------------
+# .... Working with differenced temperature series
+# ----------------------------------------------
+Temp_diff_1 = diff(Temp)
+Temp_diff_1
 
-# Checking for stationarity on the first diff
-# How to controlled -> [ The first difference of the temperature series is stationary]
-adf.test(Temp_first_diff)
+autoplot(Temp_diff_1, main="Plot of First Temperature DifferencedSeries", col="maroon", lwd=2)
 
-# --------------------- End ---------------------------
+adf.test(Temp_diff_1)
+pp.test(Temp_diff_1)
+kpss.test(Temp_diff_1)
 
-# -----------------------------------------------------
-# --------------- Plotting ACF and PACF ---------------
-# -----------------------------------------------------
-Acf(Temp) # You can assign it to a variable eg temp_afc = Acf(Temp)
+
+
+# ----------------------------------------------
+# .... Plot of ACF and PACF
+# ----------------------------------------------
+Acf(Temp)
 Pacf(Temp)
 
-# --------------------- End ---------------------------
 
 
